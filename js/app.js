f743ca341cf5b55c7c863cb32a69186332c41729
Kevin Toro <kevinandrestg7@gmail.com>
2021-09-08 22:23:41 -0500

    feat: small updates in style and index

--- js/app.js
+++ js/app.js
@@ -2,3 +2,8 @@ window.addEventListener('scroll', function () {
   const header = document.querySelector('header');
   header.classList.toggle("sticky", window.scrollY > 250);
 });
+
+function toggle_menu() {
+  const toggle = document.querySelector('.menu-toggle');
+  toggle.classList.toggle('active');
+}
